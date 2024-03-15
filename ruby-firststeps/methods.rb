2634a1d89153ca76d6e97d8d520758c173fe9458
Eduardo Rezaghi <eduardo.rezaghi@gmail.com>
2024-03-15 08:28:30 -0300

    Add more methods examples.

--- ruby-firststeps/methods.rb
+++ ruby-firststeps/methods.rb
@@ -22,4 +22,71 @@ puts my_hash[:say_hello]   #=> "Hello, world!"
 # Example 3: Globally accessible methods
 # Methods defined at the top level are globally accessible.
 # This means that you can call them from anywhere in your program.
-Object.print "Hello, world!"   #=> "Hello, world!"
+Object.send(:remove_const, :MyClass) if defined? MyClass
+def my_method
+    "This is a method."
+end
+
+p my_method   #=> "This is a method."
+
+# Example 4: Method with parameters
+def say_hello(name)
+    puts "Hello, #{name}!"
+end
+
+p say_hello("John")   #=> "Hello, John!"
+
+# Example 5: Method with default parameters
+def say_hello(name = "world")
+    puts "Hello, #{name}!"
+end
+
+p say_hello   #=> "Hello, world!"
+
+# Example 6: Method with return value
+# Explicitly return a value using the return keyword.
+def add(a, b)
+    return a + b
+end
+
+p add(2, 3)   #=> 5
+
+# Implicitly return a value using the last expression evaluated.
+def even_odd(number)
+    number % 2 == 0 ?
+      "That is an even number." :
+      "That is an odd number."
+end
+
+p even_odd(2)   #=> "That is an even number."
+p even_odd(3)   #=> "That is an odd number."
+
+# Example 7: Method with variable number of arguments
+# Use the splat operator (*) to define a method that takes a variable number of arguments.
+def add(*numbers)
+    # Here, numbers is an array.
+    # inject is a method that applies a binary operation to each element in an array.
+        # For each element in the numbers array, add it to the sum.
+    numbers.inject(0) { |sum, number| sum + number }
+end
+
+p add(1)   #=> 1
+p add(1, 2)   #=> 3
+p add(4, 5, 6, 7)  #=> 22
+
+# Example 8: Predicate methods
+# Predicate methods are methods that return a boolean value.
+# By convention, predicate methods end with a question mark (?).
+def can_divide_by?(number)
+    return false if number.zero?
+    true
+end
+
+p can_divide_by?(3)   #=> true
+p can_divide_by?(0)   #=> false
+
+# Built-in predicate methods
+p 3.even?   #=> false
+p 3.odd?   #=> true
+p [].nil?   #=> false
+p 3.nil?   #=> false
